3b10fe238fc8a7ab073eb949ce6866f5a22eccfc
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-07-13 08:54:32 +0530

    added

--- heaps/buildHeap.cpp
+++ heaps/buildHeap.cpp
@@ -1,7 +1,7 @@
 void heapify(int arr[],int n,int i) {
     int largest=i;
-    int l=2*i-1;
-    int r=2*i+1;
+    int l=2*i+1;
+    int r=2*i+2;
     if(l<n && arr[l]>arr[largest]) {
         largest=l;
     } 
@@ -18,6 +18,6 @@ void heapify(int arr[],int n,int i) {
 void buildHeap(int arr[],int n) {
     int startIndex=(n/2)-1;
     for(int i=startIndex;i>=0;i--) {
-        heapify(arr,  n,  i);
+        heapify(arr,  n-1,  i);
     }
 }
